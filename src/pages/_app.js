b9df3eefd2955fb8a5179b810c9146ffb6fd37ab
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-04-06 20:33:48 +0600

    redundant code removed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
+import CustomCursor from '@/components/CustomCursor';
 import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <CustomCursor />
+            <Component {...pageProps} />
+        </>
+    );
 }
