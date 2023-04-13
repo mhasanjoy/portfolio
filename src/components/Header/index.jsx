ee42287f6920a1be9d26466dffb1d1917af3fae3
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-04-13 16:18:00 +0600

    header updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,78 +1,81 @@
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import * as React from 'react';
+import { useState } from 'react';
 import styles from './style.module.scss';
 
 const Header = () => {
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
+    const [active, setActive] = useState(false);
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (
-            event.type === 'keydown' &&
-            (event.key === 'Tab' || event.key === 'Shift')
-        ) {
-            return;
-        }
-
-        setState({ ...state, [anchor]: open });
+    const handleMenuIcon = () => {
+        setActive(!active);
     };
 
-    const list = (anchor) => (
-        <Box
-            sx={{
-                width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250,
-            }}
-            role="presentation"
-            onClick={toggleDrawer(anchor, false)}
-            onKeyDown={toggleDrawer(anchor, false)}
-        >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
-                    (text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                {/* <ListItemIcon>
-                                    {index % 2 === 0 ? (
-                                        // <InboxIcon />
-                                    ) : (
-                                        // <MailIcon />
-                                    )}
-                                </ListItemIcon> */}
-                                <ListItemText primary={text} />
-                            </ListItemButton>
-                        </ListItem>
-                    )
-                )}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            {/* <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon> */}
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
+    // const [state, setState] = React.useState({
+    //     top: false,
+    //     left: false,
+    //     bottom: false,
+    //     right: false,
+    // });
+
+    // const toggleDrawer = (anchor, open) => (event) => {
+    //     if (
+    //         event.type === 'keydown' &&
+    //         (event.key === 'Tab' || event.key === 'Shift')
+    //     ) {
+    //         return;
+    //     }
+
+    //     setState({ ...state, [anchor]: open });
+    // };
+
+    // const list = (anchor) => (
+    //     <Box
+    //         sx={{
+    //             width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250,
+    //         }}
+    //         role="presentation"
+    //         onClick={toggleDrawer(anchor, false)}
+    //         onKeyDown={toggleDrawer(anchor, false)}
+    //     >
+    //         <List>
+    //             {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
+    //                 (text, index) => (
+    //                     <ListItem key={text} disablePadding>
+    //                         <ListItemButton>
+    //                             {/* <ListItemIcon>
+    //                                 {index % 2 === 0 ? (
+    //                                     // <InboxIcon />
+    //                                 ) : (
+    //                                     // <MailIcon />
+    //                                 )}
+    //                             </ListItemIcon> */}
+    //                             <ListItemText primary={text} />
+    //                         </ListItemButton>
+    //                     </ListItem>
+    //                 )
+    //             )}
+    //         </List>
+    //         <Divider />
+    //         <List>
+    //             {['All mail', 'Trash', 'Spam'].map((text, index) => (
+    //                 <ListItem key={text} disablePadding>
+    //                     <ListItemButton>
+    //                         {/* <ListItemIcon>
+    //                             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+    //                         </ListItemIcon> */}
+    //                         <ListItemText primary={text} />
+    //                     </ListItemButton>
+    //                 </ListItem>
+    //             ))}
+    //         </List>
+    //     </Box>
+    // );
 
     return (
         <header className={styles.header}>
-            <div className={styles.menu_btn}>
-                <div className={styles.icon}>
+            <div className={styles.menu}>
+                <div
+                    onClick={handleMenuIcon}
+                    className={`${styles.icon} ${active && styles.active}`}
+                >
                     <span></span>
                     <span></span>
                 </div>
