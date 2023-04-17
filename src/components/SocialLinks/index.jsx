163699d84d32406aa686daf8c3dc217ba3bda6b9
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-04-17 14:48:23 +0600

    updated

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -4,33 +4,26 @@ import {
     faTwitter,
 } from '@fortawesome/fontawesome-free-brands';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useEffect, useState } from 'react';
 import styles from './style.module.scss';
 
 const SocialLinks = () => {
-    const [socialLinks, setSocialLinks] = useState([]);
-
-    useEffect(() => {
-        fetch('/api/social-links')
-            .then((response) => response.json())
-            .then((data) => setSocialLinks(data))
-            .catch((error) => console.log(error));
-    }, []);
-
     return (
         <ul className={styles.social_links}>
             <li>
-                <a href={socialLinks?.[0]?.URL} target="_blank">
+                <a
+                    href="https://www.linkedin.com/in/mhasanjoy/"
+                    target="_blank"
+                >
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
             </li>
             <li>
-                <a href={socialLinks?.[1]?.URL} target="_blank">
+                <a href="https://github.com/mhasanjoy" target="_blank">
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
             </li>
             <li>
-                <a href={socialLinks?.[2]?.URL} target="_blank">
+                <a href="https://twitter.com/mhasanjoy13" target="_blank">
                     <FontAwesomeIcon icon={faTwitter} />
                 </a>
             </li>
